31de5d35ce3059da796f3d6f5eb2f954d6fccd40
RenatoDiazP <re.diazp@duocuc.cl>
2026-02-01 18:00:59 -0300

    payment and walk upload

---
+++ walks/src/main/java/com/example/walks/WalksApplication.java
@@ -0,0 +1,13 @@
+package com.example.walks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalksApplication.class, args);
+	}
+
+}
---
+++ walks/src/test/java/com/example/walks/WalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
